9afdcb65247576f05e4d0e82c4d41bcdc2eea5e4
ramajeyam-clayfin <ramajeyam@clayfin.com>
2022-07-02 18:01:40 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import About from "./Components/About/About";
+import ContactUS from "./Components/Contact/ContactUS";
+import Experience from "./Components/Experience/Experience";
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Nav/Navbar";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Services from "./Components/Services/Services";
+import Testimonials from "./Components/Testimonials/Testimonials";
 
 function App() {
   return (
     <>
       <Header/>
       <Navbar/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <ContactUS/>
+      <Footer/>
     </>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import CTA from './CTA'
 import './Header.css'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header_container'>
+        <h5>Hello I'm</h5>
+        <h1>Ramajeyam</h1>
+        <h5 className='text-light'>FrontEnd Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className='me'>
+          <img src={ME} alt='me'/>
+        </div>
+
+        <a href='#Contact' className='scroll_down'>Scroll Down</a>
+      </div>
+
+    </header>
   )
 }
 
